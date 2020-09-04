67bbc1c5a503cb577c27c68a74c870450c613762
VKormylo <kvdev@users.noreply.github.com>
2020-09-04 16:20:44 +0300

    Lesson 21 (route, navlink and dialogs page)

---
+++ src/components/Dialogs/Dialogs.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import s from './Dialogs.module.css';
+
+const Dialogs = (props) => {
+    return(
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Ihor
+                </div>
+                <div className={s.dialog}>
+                    Andrey
+                </div>
+                <div className={s.dialog}>
+                    Sveta
+                </div>
+                <div className={s.dialog}>
+                    Sasha
+                </div>
+                <div className={s.dialog}>
+                    Viktor
+                </div>
+                <div className={s.dialog}>
+                    Valera
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How is your it-kamasutra?</div>
+                <div className={s.message}>Yo</div>
+            </div>
+        </div>
+    );
+}
+
+export default Dialogs;
