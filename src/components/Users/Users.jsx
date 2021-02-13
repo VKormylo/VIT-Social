3a7b7017151f1c9f8ee1564fd9c7e9343b076128
VKormylo <vitaliykormylo@gmail.com>
2021-02-13 13:53:21 +0200

    Lesson 93 (paginator, pagination, paginator test)

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,17 +3,9 @@ import Paginator from '../common/Paginator/Paginator';
 import User from './User';
 
 let Users = ({ currentPage, onPageChanged, totalUsersCount, pageSize, users, ...props }) => {
-
-    let pagesCount = Math.ceil(totalUsersCount / pageSize);
-
-    let pages = [];
-    for (let i = 1; i <= pagesCount; i++) {
-        pages.push(i);
-    }
-
     return (
         <div>
-            <Paginator currentPage={currentPage} onPageChanged={onPageChanged} totalUsersCount={totalUsersCount} pageSize={pageSize} />
+            <Paginator currentPage={currentPage} onPageChanged={onPageChanged} totalItemsCount={totalUsersCount} pageSize={pageSize} />
             <div>
             {
                 users.map(u => <User user={u} key={u.id} followingInProgress={props.followingInProgress} unfollow={props.unfollow} follow={props.follow} />)
