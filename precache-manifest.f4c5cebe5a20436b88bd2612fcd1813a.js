4a7aacb73c35b8993b81c1b3fd2f78aedd209fda
VKormylo <vitaliykormylo@gmail.com>
2021-02-14 14:12:29 +0200

    Updates

---
+++ precache-manifest.f4c5cebe5a20436b88bd2612fcd1813a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a95c35be55c96a1d94422269bbacaaaa",
+    "url": "/VIT-Social/index.html"
+  },
+  {
+    "revision": "82e772b40e4cb2f7a03a",
+    "url": "/VIT-Social/static/css/3.db2bea94.chunk.css"
+  },
+  {
+    "revision": "c5d172290f901dda1753",
+    "url": "/VIT-Social/static/css/4.9b4012a3.chunk.css"
+  },
+  {
+    "revision": "20997cb97773473c8389",
+    "url": "/VIT-Social/static/css/main.50f4c307.chunk.css"
+  },
+  {
+    "revision": "b73c68001dc2bdb0d4dc",
+    "url": "/VIT-Social/static/js/2.2fe3d0b7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/VIT-Social/static/js/2.2fe3d0b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82e772b40e4cb2f7a03a",
+    "url": "/VIT-Social/static/js/3.bc81a385.chunk.js"
+  },
+  {
+    "revision": "c5d172290f901dda1753",
+    "url": "/VIT-Social/static/js/4.66007e93.chunk.js"
+  },
+  {
+    "revision": "20997cb97773473c8389",
+    "url": "/VIT-Social/static/js/main.f2476b68.chunk.js"
+  },
+  {
+    "revision": "0fc4aaf0277974d0e737",
+    "url": "/VIT-Social/static/js/runtime-main.0e3c4df5.js"
+  },
+  {
+    "revision": "37376522ea90cb9b35283b6260e78015",
+    "url": "/VIT-Social/static/media/loader.37376522.gif"
+  },
+  {
+    "revision": "b6681619dcc616623083589aa826d2be",
+    "url": "/VIT-Social/static/media/user-profile.b6681619.webp"
+  },
+  {
+    "revision": "0056747796b55084fa2b58a171787c50",
+    "url": "/VIT-Social/static/media/user.00567477.png"
+  }
+]);
