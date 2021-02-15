d83c5ee75bd5aa9a7602216972e79acbc36c6aec
VKormylo <vitaliykormylo@gmail.com>
2021-02-15 17:30:29 +0200

    Lesson 96 (load image, shouldComponentUpdate fix)

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -4,16 +4,23 @@ import userPhoto from '../../../assets/images/user-profile.webp';
 import Preloader from '../../common/preloader/preloader';
 import ProfileStatusWithHooks from './ProfileStatusWithHooks';
 
-const ProfileInfo = ({profile, status, updateStatus}) => {
+const ProfileInfo = ({profile, status, updateStatus, isOwner, savePhoto}) => {
 
     if (!profile) {
         return <Preloader />
     }
 
+    const onMainPhotoSelected = (e) => {
+        if (e.target.files.length) {
+            savePhoto(e.target.files[0]);
+        }
+    }
+
     return (
         <div>
             <div className={s.descriptionBlock}>
-                <img src={profile.photos.large != null ? profile.photos.large : userPhoto} className={s.userPhoto} />
+                <img src={profile.photos.large || userPhoto} className={s.mainPhoto} />
+                { isOwner && <input type="file" onChange={onMainPhotoSelected} /> }
                 <div>{profile.aboutMe}</div>
                 <ProfileStatusWithHooks status={status} updateStatus={updateStatus} />
             </div>
